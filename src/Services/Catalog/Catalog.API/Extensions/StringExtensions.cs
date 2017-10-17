b3c6f7d2342eb0d2b6e6fa914fbf61a56401d18f
fran pérez <fran.perez@turingchallenge.com>
2017-10-17 20:00:01 +0200

    new controller for catalog AI operations  * dump catalog to csv  * recommendation client string extensions for CSV formatting

---
+++ src/Services/Catalog/Catalog.API/Extensions/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Catalog.API.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string FormatAsCSV<T>(this IEnumerable<T> value) where T : class
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            var properties = typeof(T).GetProperties();
+            stringBuilder.AppendLine(String.Join(",", properties.Select(p => p.Name)));
+            foreach (var csvLine in value)
+            {
+                var columnValues = properties
+                    .Select(p => p.GetValue(csvLine).ToString())
+                    .Select(p => p.FormatAsCSV());
+
+                stringBuilder.AppendLine(String.Join(",", columnValues));
+            }
+            return stringBuilder.ToString();
+        }
+
+        public static bool IsValidCSV(this string value)
+        {
+            return value.IndexOfAny(new char[] { '"', ',' }) == -1;
+        }
+
+        public static string FormatAsCSV(this string value)
+        {
+            return value.IsValidCSV() ?
+                value :
+                String.Format("\"{0}\"", value.Replace("\"", "\"\""));
+        }
+    }
+}
