f7918b1121e7adf5839065d840e5cdc581253c86
fran pérez <fran.perez@turingchallenge.com>
2017-10-24 16:47:36 +0200

    add get recommendations service

---
+++ src/Web/WebMVC/Services/ICatalogAIService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.eShopOnContainers.WebMVC.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopOnContainers.WebMVC.Services
+{
+    public interface ICatalogAIService
+    {
+        Task<List<CatalogItem>> GetRecommendationsAsync(string productId);
+    }
+}
