20108a03f884759846a55eed31285107ffac9efc
fran pérez <fran.perez@turingchallenge.com>
2017-10-24 16:49:25 +0200

    add view component for displaying product recommendations in basket

---
+++ src/Web/WebMVC/ViewComponents/RecommendationList.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.eShopOnContainers.WebMVC.Services;
+using Microsoft.eShopOnContainers.WebMVC.ViewModels;
+using Polly.CircuitBreaker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.ViewComponents
+{
+    public class RecommendationList : ViewComponent
+    {
+        private readonly ICatalogAIService _catalogAIService;
+
+        public RecommendationList(ICatalogAIService catalogAIService) => _catalogAIService = catalogAIService;
+
+        public async Task<IViewComponentResult> InvokeAsync(string productId)
+        {
+            const int maxProductRecommendations = 3;
+
+            if (String.IsNullOrEmpty(productId))
+                return View();
+
+            try
+            {
+                var recommendations = await _catalogAIService.GetRecommendationsAsync(productId);
+                return View(recommendations.Take(maxProductRecommendations).ToList());
+
+            } catch (BrokenCircuitException)
+            {
+                TempData["RecommendationInoperativeMsg"] = "Recommendation Service is inoperative, please try later on. (Business Msg Due to Circuit-Breaker)";
+            }
+
+            return View();
+        }
+    }
+}
