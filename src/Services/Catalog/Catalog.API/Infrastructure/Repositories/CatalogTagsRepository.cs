a335a5c16574e607f8ddcebb2fe190aedf72b6c9
fran pérez <fran.perez@turingchallenge.com>
2017-11-08 00:28:01 +0100

    add catalog tags model add repository supporting catalog tags in mongodb add catalog tag seed, for initializing db collection

---
+++ src/Services/Catalog/Catalog.API/Infrastructure/Repositories/CatalogTagsRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.eShopOnContainers.Services.Catalog.API;
+using Microsoft.eShopOnContainers.Services.Catalog.API.Model;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Infrastructure
+{
+    public class CatalogTagsRepository : ICatalogTagsRepository
+    {
+        private readonly CatalogTagsContext dbContext;
+
+        public CatalogTagsRepository(IOptionsSnapshot<CatalogSettings> settings)
+        {
+            dbContext = new CatalogTagsContext(settings);
+        }
+
+        public bool Empty => dbContext.CatalogTags.Count(Builders<CatalogTag>.Filter.Empty) == 0;
+
+        public IEnumerable<CatalogTag> FindMatchingCatalogTag(IEnumerable<string> tags)
+        {
+            var filter = Builders<CatalogTag>.Filter.AnyIn(x => x.Tagrams, tags);
+
+            // ASYNC ?? 
+            return dbContext.CatalogTags
+                .Find(filter)
+                .ToEnumerable();
+        }
+
+        public async Task Insert(IEnumerable<CatalogTag> catalogTags)
+        {
+            // InsertManyAsync uses internally BulkWriteAsync
+            // https://stackoverflow.com/questions/32921533/mongodb-c-sharp-driver-2-0-insertmanyasync-vs-bulkwriteasync
+            await dbContext.CatalogTags.InsertManyAsync(catalogTags);
+        }
+
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Infrastructure/Repositories/ICatalogTagsRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.eShopOnContainers.Services.Catalog.API.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Infrastructure
+{
+    public interface ICatalogTagsRepository
+    {
+        IEnumerable<CatalogTag> FindMatchingCatalogTag(IEnumerable<string> tags);
+        Task Insert(IEnumerable<CatalogTag> catalogTags);
+        bool Empty { get; }
+    }
+}
