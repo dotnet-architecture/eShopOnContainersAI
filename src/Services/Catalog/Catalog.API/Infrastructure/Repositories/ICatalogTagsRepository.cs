2b099db63e3e339f3b7b2df62535adde450c2432
franperezlopez <1222398+franperezlopez@users.noreply.github.com>
2018-04-19 19:50:31 +0200

    add method CatalogAI for getting similar products based on description update dumptocsv method, accepts format parameter (json/csv)

--- src/Services/Catalog/Catalog.API/Infrastructure/Repositories/ICatalogTagsRepository.cs
+++ src/Services/Catalog/Catalog.API/Infrastructure/Repositories/ICatalogTagsRepository.cs
@@ -7,7 +7,7 @@ namespace Catalog.API.Infrastructure
     public interface ICatalogTagsRepository
     {
         Task<List<CatalogTag>> FindMatchingTagsAsync(IEnumerable<string> tags);
-        Task<List<CatalogTag>> FindMatchingProductsAsync(IEnumerable<int> productIds);
+        Task<List<CatalogFullTag>> FindMatchingProductsAsync(IEnumerable<int> productIds);
         Task InsertAsync(IEnumerable<CatalogFullTag> catalogTags);
         bool IsEmpty { get; }
         Task<List<CatalogFullTag>> All { get; }
