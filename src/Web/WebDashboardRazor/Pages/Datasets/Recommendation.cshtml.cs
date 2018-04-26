5c08aa91042bd06494bcc8c292478ac81ab6d9fd
franperezlopez <1222398+franperezlopez@users.noreply.github.com>
2018-04-26 10:58:00 +0200

    new UI

---
+++ src/Web/WebDashboardRazor/Pages/Datasets/Recommendation.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.eShopOnContainers.WebDashboardRazor.Infrastructure;
+using Microsoft.eShopOnContainers.WebDashboardRazor.Models;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.eShopOnContainers.WebDashboardRazor.Pages.Datasets
+{
+    public class RecommendationModel : PageModel
+    {
+        private readonly AppSettings appSettings;
+
+        public RecommendationModel(IOptionsSnapshot<AppSettings> appSettings)
+        {
+            this.appSettings = appSettings.Value;
+        }
+
+        public string CustomerUrl { get; private set; }
+        public string ProductUrl { get; private set; }
+        public string OrderItemsUrl { get; set; }
+
+        public void OnGet()
+        {
+            ViewData.SetSelectedMenu(SelectedMenu.Data_SalesByCountry);
+
+            CustomerUrl = API.Identity.UserInfo(appSettings.WebShoppingUrl);
+            ProductUrl = API.Catalog.ProductInfo(appSettings.WebShoppingUrl, "csv");
+            OrderItemsUrl = API.Ordering.CountryStats(appSettings.WebShoppingUrl);
+        }
+    }
+}
