9dd73ca1360f81a89a937fa861107860a96160e2
franperezlopez <1222398+franperezlopez@users.noreply.github.com>
2018-05-16 11:27:08 +0200

    add forecasting training project

---
+++ src/Services/AI.SalesForecasting/Training/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopOnContainers.Services.AI.SalesForecasting.Training.MLNet.API
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                await CountryModelHelper.SaveModel("data/countries.stats.csv");
+                await CountryModelHelper.TestPrediction();
+                await ProductModelHelper.SaveModel("data/products.stats.csv");
+                await ProductModelHelper.TestPrediction();
+            } catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+        }
+    }
+}
