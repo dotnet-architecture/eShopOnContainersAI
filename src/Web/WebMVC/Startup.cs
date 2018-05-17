90e37448d5f6d96be0960a54eaa8a2db4b60fdd0
franperezlopez <1222398+franperezlopez@users.noreply.github.com>
2018-05-17 16:53:48 +0200

    merge from upstream  * signalR update update CSV files

--- src/Web/WebMVC/Startup.cs
+++ src/Web/WebMVC/Startup.cs
@@ -135,6 +135,7 @@ namespace Microsoft.eShopOnContainers.WebMVC
                 options.Scope.Add("marketing");
                 options.Scope.Add("locations");
                 options.Scope.Add("webshoppingagg");
+                options.Scope.Add("orders.signalrhub");
             });
         }
 
