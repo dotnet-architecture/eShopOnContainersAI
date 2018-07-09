1ce2120562f491135c824dd9d41cc70fc6e48294
franperezlopez <1222398+franperezlopez@users.noreply.github.com>
2018-07-09 14:19:23 +0200

    add comments for data entities

--- src/Services/AI.SalesForecasting/Training/CountryData.cs
+++ src/Services/AI.SalesForecasting/Training/CountryData.cs
@@ -4,29 +4,62 @@ namespace Microsoft.eShopOnContainers.Services.AI.SalesForecasting.Training.MLNe
 {
     public class CountryData
     {
-        // next,country,year,month,max,min,std,count,sales,med,prev
+        /// <summary>
+        /// This represent next period country sales. Sales are measured in dollars in logaithmic scale. Period is measured in month/unit
+        /// MLNet needs this column to be named as "Label"
+        /// </summary>
         [Column(ordinal: "0", name: "Label")]
         public float next;
 
+        /// <summary>
+        /// Contry ID
+        /// </summary>
         [Column(ordinal: "1")]
         public string country;
 
+        /// <summary>
+        /// Period year
+        /// </summary>
         [Column(ordinal: "2")]
         public float year;
+        /// <summary>
+        /// Period month
+        /// </summary>
         [Column(ordinal: "3")]
         public float month;
+        /// <summary>
+        /// 80th percentile sale in current period
+        /// </summary>
         [Column(ordinal: "4")]
         public float max;
+        /// <summary>
+        /// 20th percentile sale in current period
+        /// </summary>
         [Column(ordinal: "5")]
         public float min;
+        /// <summary>
+        /// Sale standard deviation in current period
+        /// </summary>
         [Column(ordinal: "6")]
         public float std;
+        /// <summary>
+        /// Number of sales in current period
+        /// </summary>
         [Column(ordinal: "7")]
         public float count;
+        /// <summary>
+        /// Sum of sales in current period
+        /// </summary>
         [Column(ordinal: "8")]
         public float sales;
+        /// <summary>
+        /// Sale median (50th percentile) in current period
+        /// </summary>
         [Column(ordinal: "9")]
         public float med;
+        /// <summary>
+        /// Previous period sale
+        /// </summary>
         [Column(ordinal: "10")]
         public float prev;
     }
--- src/Services/AI.SalesForecasting/Training/ProductData.cs
+++ src/Services/AI.SalesForecasting/Training/ProductData.cs
@@ -2,29 +2,62 @@
 
 namespace Microsoft.eShopOnContainers.Services.AI.SalesForecasting.Training.MLNet.API
 {
+    /// <summary>
+    /// Sample for sales prediction by product model
+    /// </summary>
     public class ProductData
     {
-        // next,productId,year,month,units,avg,count,max,min,prev
+        /// <summary>
+        /// This represents next period sales. Sales are measured in units sold. Period is measured in month/unit
+        /// MLNet needs this column to be named as "Label"
+        /// </summary>
         [Column(ordinal: "0", name: "Label")]
         public float next;
 
+        /// <summary>
+        /// Product ID
+        /// </summary>
         [Column(ordinal: "1")]
         public string productId;
 
+        /// <summary>
+        /// Period year
+        /// </summary>
         [Column(ordinal: "2")]
         public float year;
+        /// <summary>
+        /// Period month
+        /// </summary>
         [Column(ordinal: "3")]
         public float month;
+        /// <summary>
+        /// Sales in current period
+        /// </summary>
         [Column(ordinal: "4")]
         public float units;
+        /// <summary>
+        /// Mean sales in current period
+        /// </summary>
         [Column(ordinal: "5")]
         public float avg;
+        /// <summary>
+        /// Number of sales in current period
+        /// </summary>
         [Column(ordinal: "6")]
         public float count;
+        /// <summary>
+        /// Max products sold in a single sale in current period
+        /// </summary>
         [Column(ordinal: "7")]
         public float max;
+        /// <summary>
+        /// Min products sold in a single sale in current period
+        /// </summary>
         [Column(ordinal: "8")]
         public float min;
+        /// <summary>
+        /// Previous period sales
+        /// </summary>
         [Column(ordinal: "9")]
         public float prev;
     }
