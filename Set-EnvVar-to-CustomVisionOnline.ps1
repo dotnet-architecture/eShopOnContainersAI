e72c82c4907ae8752d065f54d4b68bbb906099cf
Cesar De la Torre Llorente <cesardl@microsoft.com>
2018-09-06 16:46:49 -0700

    Added Scripts to set approach for the Image-Based Product Search

---
+++ Set-EnvVar-to-CustomVisionOnline.ps1
@@ -0,0 +1,16 @@
+Write-Host "Initial ESHOPAI_PRODUCTSEARCHIMAGEBASED_APPROACH value: " $env:ESHOPAI_PRODUCTSEARCHIMAGEBASED_APPROACH
+
+$env:ESHOPAI_PRODUCTSEARCHIMAGEBASED_APPROACH = "CustomVisionOnline" 
+
+Write-Host "Final ESHOPAI_PRODUCTSEARCHIMAGEBASED_APPROACH value: " $env:ESHOPAI_PRODUCTSEARCHIMAGEBASED_APPROACH
+
+Write-Host "Stopping and deleting the containers..."
+docker-compose rm --stop --force ai.productsearchimagebased.tensorflow.api 
+docker-compose rm --stop --force ai.productsearchimagebased.azurecognitiveservices.api 
+docker-compose rm --stop --force webmvc 
+
+Write-Host "Starting the containers..."
+docker-compose up --no-deps -d ai.productsearchimagebased.tensorflow.api
+docker-compose up --no-deps -d ai.productsearchimagebased.azurecognitiveservices.api
+docker-compose up --no-deps -d webmvc
+
