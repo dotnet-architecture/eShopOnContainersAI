5fad8951c0c3087b0ddaa89985cea7080f57853a
franperezlopez <1222398+franperezlopez@users.noreply.github.com>
2018-10-19 10:19:52 +0200

    remove Bot v.3 project

--- src/Bots/Bot.API/Models/Catalog/CatalogType.cs
+++
@@ -1,9 +0,0 @@
-namespace Microsoft.Bots.Bot.API.Models.Catalog
-{
-    public class CatalogType
-    {
-        public string Id { get; set; }
-        public string Text { get; set; }
-        public bool IsSelected {get;set;}
-    }
-}
--- src/Bots/Bot.API/Models/Order/OrderDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Microsoft.Bots.Bot.API.Models.Order
-{
-    public class OrderDTO
-    {
-        public string OrderNumber { get; set; }
-    }
-}
